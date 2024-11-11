ec6b0fad5245aa9e32bc1b72b2c591e0f865c960
jarallum92 <jamieallum92@gmail.com>
2024-11-11 12:27:19 -0500

    Added updated cipher file

--- cipher.py
+++ cipher.py
@@ -1,1 +1,20 @@
 # add your code here
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def caesar_cipher(text, shift):
+    encrypted_sentence = ""
+
+
+    for char in sentence:
+        if char.islower():
+            encrypted_sentence += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            encrypted_sentence += char
+
+    return encrypted_sentence
+    
+shift = 5
+sentence = input("Please enter a sentence: ")
+text = sentence.lower()
+
+print("The encrpyted sentece is:", caesar_cipher(text, shift))
